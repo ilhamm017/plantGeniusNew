b0b9b7ac10a5b42c0da1a995c5943a6b82dec8ad
ilhammuliawan26 <ilhammuliawan26@gmail.com>
2024-06-06 23:53:37 +0000

    minor update

--- user-service/index.js
+++ user-service/index.js
@@ -3,7 +3,7 @@ const app = express()
 const route = require('./routes/routes')
 const port = process.env.PORT || 3000
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: true}))
 
 app.use('/user', route)
 
